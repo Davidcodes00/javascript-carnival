d8dc815457dc7702c25335f93e57fa5ff9f59a78
Davidcode00 <Davidcodes00@gmail.com>
2022-05-16 14:45:07 +1200

    declaring-cariables

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -1,5 +1,17 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Dress The Clown!")
+console.log('Dress The Clown!')
+
+//declaring variables for body,head and shoes
+
+let head = document.getElementById('head')
+let body = document.getElementById('body')
+let shoes = document.getElementById('shoes')
+
+// declaring index for above
+let headIndex = 0
+let bodyIndex = 0
+let shoesIndex = 0
+let clothesIndex = 0
