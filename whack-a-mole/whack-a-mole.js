ae45cadb051d90fe84bbe0864889f72ecc174ace
Davidcode00 <Davidcodes00@gmail.com>
2022-05-16 14:31:11 +1200

    halfway-done-need-function

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,19 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+console.log('Whack-a-Mole!')
+
+let cells = document.getElementsByTagName('td')
+
+let mole = document.createElement('img')
+mole.src = './mole.PNG'
+mole.id = 'mole'
+
+//variables for different random numbers
+let min = 0
+let max = 24
+let number = randomNumber()
+let random = cells[number]
+
+let audio = new Audio('./whack-audio.wav')
